1fa5b922da6061f3bafdc281ba17209b351bb7b4
mammaliga <93888804+mammaliga@users.noreply.github.com>
2024-02-09 16:37:42 +0300

    Update main.py

--- python_scripts/CSV/weather/main.py
+++ python_scripts/CSV/weather/main.py
@@ -4,13 +4,7 @@ data = pd.read_csv("weather_data.csv")
 
 temperatures = data["temp"].to_list()
 avg_temp = data["temp"].mean()
-# print(avg_temp)
 max_temp = data["temp"].max()
-# print(max_temp)
-
-# print(data[data["day"] == "Tuesday"])
-# print(data[data.temp == max_temp])
-
 monday = data[data.day == "Monday"]
 monday_temp = int(monday.temp)
 monday_temp_F = (monday_temp * (9/5)) + 32
